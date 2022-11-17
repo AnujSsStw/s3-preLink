37d25fedcc144da4703d40596255f3de143da201
Anuj SIngh <ani_@pop-os.localdomain>
2022-11-17 20:50:15 +0530

    done

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import S3 from "aws-sdk/clients/s3";
+import { randomUUID } from "crypto";
 
-type Data = {
-  name: string
-}
+const s3 = new S3({
+  apiVersion: "2006-03-01",
+  region: process.env.REGION,
+  secretAccessKey: process.env.SECRET_KEY,
+  accessKeyId: process.env.ACCESS_KEY,
+  signatureVersion: "v4",
+});
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const extension = (req.query.fileType as string).split("/")[1];
+  const Key = `${randomUUID()}.${extension}`;
+
+  const s3Params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key,
+    Expires: 60,
+    ContentType: `image/${extension}`,
+  };
+
+  const uploadURL = s3.getSignedUrl("putObject", s3Params);
+  res.status(200).json({
+    uploadURL,
+    key: Key,
+  });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import axios from "axios";
+import Head from "next/head";
+import Image from "next/image";
+import { ChangeEvent } from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const doYOURthing = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const file = formData.get("file");
+    if (!file) {
+      return;
+    }
+
+    // @ts-ignore
+    const fileType = encodeURIComponent(file.type);
+
+    const { data } = await axios.get(`/api/hello?fileType=${fileType}`);
+    const { uploadURL, key } = data;
+
+    await axios.put(uploadURL, file);
+
+    return key;
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -12,60 +32,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <form onSubmit={doYOURthing}>
+          <input
+            type="file"
+            accept="image/jpeg image/png image/svg"
+            name="file"
+          />
+          <button type="submit">submit</button>
+        </form>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
